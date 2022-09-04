4a9f743441894b730392cf8ef72f38d5dadb1255
jialei <jialei@shanghaitech.edu.cn>
2022-09-04 22:54:47 +0800

    Init

---
+++ docs/_next/static/_jmD35fHtTU5DMmYwgl5K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a74c42cf68dcff47.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: '/personal-blog',
+}
+
+module.exports = nextConfig
