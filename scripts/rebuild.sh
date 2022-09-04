ffd72958d3dd20b1f14c7d7f9b57f04ef1f1cb80
jialei <jialei@shanghaitech.edu.cn>
2022-09-04 23:27:25 +0800

    update contents

---
+++ scripts/rebuild.sh
@@ -0,0 +1,10 @@
+rm -rf node_modules
+rm package-lock.json
+
+# clear cache
+# npm cache clear --force
+
+# use domestic source
+# npm config set registry [https://registry.npm.taobao.org](https://link.jianshu.com/?t=https://registry.npm.taobao.org)
+
+npm install
