e957d02eed7e56d850bdb81fd70bc5a76993e968
jialei <jialei@shanghaitech.edu.cn>
2022-09-06 21:41:04 +0800

    clean contents

--- components/showList.js
+++ components/showList.js
@@ -1,6 +1,6 @@
 export default function showList(props){
    const str = props.list.map(
-      (filename,idx)=><p>{filename} {idx}</p>
+      (filename,idx)=><p key={idx}>{filename} {idx}</p>
    );
    return str;
 }
