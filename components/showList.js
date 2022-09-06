9f0fef807aa572129122e13dd8dd5f9f5437ffa5
jialei <jialei@shanghaitech.edu.cn>
2022-09-06 23:58:33 +0800

    clean contents

--- components/showList.js
+++ components/showList.js
@@ -4,9 +4,9 @@ import path from 'path';
 export default function showList(props){
    const str = props.list.map(
       (filename,idx)=>
-         <div className="text-center">
-            <Link href={path.join('/posts',filename)} key={idx}>
-               <a key={idx}><h1 key={idx}>{filename.substring(0,filename.length-3)}</h1></a>
+         <div className="text-center" key={idx}>
+            <Link href={path.join('/posts',filename)} >
+               <a><h1>{filename.substring(0,filename.length-3)}</h1></a>
             </Link>
          </div>
    );
