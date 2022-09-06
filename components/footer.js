704d40f79bdf8bfce954cbda56ce98c44062a1ff
jialei <jialei@shanghaitech.edu.cn>
2022-09-06 14:22:57 +0800

    clean contents

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
    return (
       <div>
-         <div class="flex flex-col sm:flex-row items-center sm:justify-between pt-10 sm:pt-12">
+         <div className="flex flex-col sm:flex-row items-center sm:justify-between pt-10 sm:pt-12">
             <h1>Todo: svg icon</h1>
             <h1>This website is for personal learning</h1>
          </div>
--- components/header.js
+++ components/header.js
@@ -1,29 +1,36 @@
+import Link from 'next/link'
+
 export default function Header() {
    return (
       <div>
          <div className="sticky top-0 z-40 w-full h-20 bg-header bg-top flex-none flex space-x-4">
-            <div class="flex-none pl-4 flex items-center border-b border-gray-200">
-               <a class="overflow-hidden w-auto" href="/">
-                  <h1>Todo: svg icon</h1>
-               </a>
+            <div className="flex-none pl-4 flex items-center border-b border-gray-200">
+               <div className="overflow-hidden w-auto">
+                  <Link href="/">
+                     <h1>Todo: svg icon</h1>
+                  </Link>
+               </div>
             </div>
-            <button type="button" class="group leading-6 font-medium flex items-center space-x-3 sm:space-x-4 hover:text-gray-600 transition-colors duration-200 w-full py-2">
-               <svg width="24" height="24" fill="none" class="text-gray-400 group-hover:text-gray-500 transition-colors duration-200">
-                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  </path>
-               </svg>
-               <span>Todo:搜索<span class="hidden sm:inline"> 一切</span></span>
+            <button type="button" className="group leading-6 font-medium flex items-center space-x-3 sm:space-x-4 hover:text-gray-600 transition-colors duration-200 w-full py-2">
+               <span>Todo:搜索icon<span className="hidden sm:inline"> 一切</span></span>
+               <span>搜索一切</span>
             </button>
-            <div class="flex items-center space-x-6 ml-6 w-30">
-               <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10" href="/">
-                  <span class="inline">首页</span>
-               </a>
-               <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10" href="/posts">
-                  <span class="inline">文档</span>
-               </a>
-               <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10" href="/posts">
-                  <span class="inline">计划</span>
-               </a>
+            <div className="flex items-center space-x-6 ml-6 w-30">
+               <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
+                  <Link href="/">
+                     <span className="inline">首页</span>
+                  </Link>
+               </div>
+               <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
+                  <Link href="/posts">
+                     <span className="inline">文档</span>
+                  </Link>
+               </div>
+               <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
+                  <Link href="/">
+                     <span className="inline">计划</span>
+                  </Link>
+               </div>
                </div>
          </div>
       </div>
