e1af34c0d51655cab0a53bc75ccdd723b274c54b
jialei <jialei@shanghaitech.edu.cn>
2022-09-06 23:56:26 +0800

    clean contents

--- components/showList.js
+++ components/showList.js
@@ -6,7 +6,7 @@ export default function showList(props){
       (filename,idx)=>
          <div className="text-center">
             <Link href={path.join('/posts',filename)} key={idx}>
-               <a><h1>{filename.substring(0,filename.length-3)}</h1></a>
+               <a key={idx}><h1 key={idx}>{filename.substring(0,filename.length-3)}</h1></a>
             </Link>
          </div>
    );
