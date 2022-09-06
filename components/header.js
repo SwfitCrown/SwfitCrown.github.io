d6cef1660e82c23b53f620ba8b5d1ae5a9ce8be8
jialei <jialei@shanghaitech.edu.cn>
2022-09-06 14:32:44 +0800

    clean contents

--- components/header.js
+++ components/header.js
@@ -12,23 +12,29 @@ export default function Header() {
                </div>
             </div>
             <button type="button" className="group leading-6 font-medium flex items-center space-x-3 sm:space-x-4 hover:text-gray-600 transition-colors duration-200 w-full py-2">
-               <span>Todo:搜索icon<span className="hidden sm:inline"> 一切</span></span>
+               <span>Todo:icon<span className="hidden sm:inline"> 一切</span></span>
                <span>搜索一切</span>
             </button>
             <div className="flex items-center space-x-6 ml-6 w-30">
                <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
                   <Link href="/">
-                     <span className="inline">首页</span>
+                     <a>
+                        <span className="inline">首页</span>
+                     </a> 
                   </Link>
                </div>
                <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
                   <Link href="/posts">
-                     <span className="inline">文档</span>
+                     <a>
+                        <span className="inline">文档</span>
+                     </a> 
                   </Link>
                </div>
                <div className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 w-10">
                   <Link href="/">
-                     <span className="inline">计划</span>
+                     <a>
+                        <span className="inline">计划</span>
+                     </a> 
                   </Link>
                </div>
                </div>
